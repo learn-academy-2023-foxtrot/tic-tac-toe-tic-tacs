ea046a3c855730fe30eba6f2c4674ccb2c0870a4
Adrian Ramirez <adrian.ramirez45679@gmail.com>
2023-09-19 04:01:39 -0700

    few new changes

--- src/App.js
+++ src/App.js
@@ -1,84 +1,96 @@
-import React, { useState } from 'react'
-import './App.css'
+import React, { useState } from 'react';
+import './App.css';
 
 const App = () => {
-  const [squares, setSquares] = useState(Array(9).fill(null)) 
-  const [currentPlayer, setCurrentPlayer] = useState("X")
-  const [winner, setWinner] = useState(null)
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  const [currentPlayer, setCurrentPlayer] = useState("X");
+  const [winner, setWinner] = useState(null);
 
   const calculateWinner = (squares) => {
     const winningCombinations = [
-      [0,1,2],
-      [3,4,5],
-      [6,7,8],
-      [0,3,6],
-      [1,4,7],
-      [2,5,8],
-      [0,4,8],
-      [2,4,6]
-    ]; 
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+
     for (let i = 0; i < winningCombinations.length; i++) {
       const [a, b, c] = winningCombinations[i];
       if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
         return squares[a];
       }
     }
-    if(!squares.includes(null)){
-      return "draw"
-    }  
-    return null
-  }
+
+    if (areAllSquaresFilled(squares)) {
+      return "draw";
+    }
+
+    return null;
+  };
+
+  const areAllSquaresFilled = (squares) => {
+    return squares.every((square) => square !== null);
+  };
 
   const getStatusMessage = () => {
-    if(winner){
-      return `${winner}`
-    }else if(!squares.includes(null)){
-      return "It's a draw!"
-    }else{
-      return `${currentPlayer}`
+    if (winner) {
+      return `${winner}`;
+    } else if (areAllSquaresFilled(squares)) {
+      return "It's a draw!";
+    } else {
+      return `${currentPlayer}`;
     }
-  }
+  };
 
   const handleSquareClick = (i) => {
-    if(squares[i] || winner) return ;
+    if (squares[i] || winner) {
+      return;
+    }
 
-    const newSquares = [...squares] ;
-      newSquares[i] = currentPlayer ;
-      setSquares(newSquares)
+    const newSquares = [...squares];
+    newSquares[i] = currentPlayer;
+    setSquares(newSquares);
 
-      const newWinner = calculateWinner(newSquares)
-      if(newWinner){
-        setWinner(newWinner)
-      }else{
-        setCurrentPlayer(currentPlayer === "X" ? "O" : "X")
-      }
-  }
+    const newWinner = calculateWinner(newSquares);
+    if (newWinner) {
+      setWinner(newWinner);
+    } else {
+      setCurrentPlayer(currentPlayer === "X" ? "O" : "X");
+    }
+  };
 
   const handleRestartClick = () => {
-    setSquares(Array(9).fill(null))
-    setCurrentPlayer("X")
-    setWinner(null)
-  }
-   
-  
+    setSquares(Array(9).fill(null));
+    setCurrentPlayer("X");
+    setWinner(null);
+  };
+
+  const renderSquares = () =>
+    squares.map((value, i) => (
+      <button
+        key={i}
+        className="square"
+        onClick={() => handleSquareClick(i)}
+        disabled={value || winner}
+      >
+        {value}
+      </button>
+    ));
+
   return (
     <div className="App">
-      <h1 className='title'>Tic Tac Toe</h1>
-      <div className="board">
-        {squares.map((value, i)=> (
-          <button key={i} className="square" onClick={() => handleSquareClick(i)} disabled={value || winner}>
-            {value}
-          </button>
-        ))}
-      </div>
-      <div className="status"> 
-        {getStatusMessage()}
-      </div>
-      <button className="reset-button" onClick={handleRestartClick}> 
-          Restart Game
+      <h1 className="title">Tic Tac Toe</h1>
+      <div className="board">{renderSquares()}</div>
+      <div className="status">{getStatusMessage()}</div>
+      <button className="reset-button" onClick={handleRestartClick}>
+        Restart Game
       </button>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
